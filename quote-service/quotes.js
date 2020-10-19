90987380935aa81b17000db135f859a3a68a5cac
Aman Patel <amanptl@users.noreply.github.com>
2020-10-19 13:19:04 -0400

    Integrated 3rd party API.

--- quote-service/quotes.js
+++ quote-service/quotes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var request = require('request');
 
 const app = express();
 
@@ -9,6 +10,42 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.status(200).send("Server is running")
 });
+
+app.get('/quotes', (req, res) => {
+  const tag = req.param.tag
+  const options = {
+      url: 'https://api.paperquotes.com/apiv1/quotes/?tags=hair,potrait&curated=1',
+      headers: {
+        'Authorization': 'Token 46512a1ab44bdcaf07dbc9ff7707634d086cdf7e'
+      },
+      params: {
+        'tags': tag
+      }
+    };
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        res.send(body)
+      };
+      res.send('Something went wrong.')
+    });
+});
+
+function getQuotes(tag) {
+  const options = {
+    url: 'https://api.paperquotes.com/apiv1/quotes/?tags=hair,potrait&curated=1',
+    headers: {
+      'Authorization': 'Token 46512a1ab44bdcaf07dbc9ff7707634d086cdf7e'
+    },
+    params: {
+      'tags': tag
+    }
+  };
+  request(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      
+    };
+  });
+};
 
 app.get('*', (req, res) => {
   res.status(404).send('Not Found')
